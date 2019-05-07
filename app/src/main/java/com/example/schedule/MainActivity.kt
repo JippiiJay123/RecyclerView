56e2faec6df8182cb5a2e69e8d8e35a18fd11d0a
Victor <VictorWong132@gmail.com>
2019-05-06 17:55:17 -0700

    Hello World

--- app/src/main/java/com/example/schedule/MainActivity.kt
+++ app/src/main/java/com/example/schedule/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.schedule
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+
+        val users = ArrayList<User>()
+
+        users.add(User("BRUCE LEE", "Vancouver"))
+
+        val adapter = CustomAdapter(users)
+
+        recyclerView.adapter = adapter
     }
 }
